162c34cf202e0302f99ab0eafc92ba79e3f3cd69
vadimka-8888 <73032993+vadimka-8888@users.noreply.github.com>
2022-03-05 21:30:30 +0300

    Add new method

---
+++ PetCareDB/PetCareDB/PetCareMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetCareDB.EF;
+
+namespace PetCareDB
+{
+    public static class PetCareMethods
+    {
+        public static int AddNewUser(string fname, string lname, string email, string password, string district, bool confirmation)
+        {
+            using (PetCareEntities context = new PetCareEntities())
+            {
+                try
+                {
+                    var user = new User
+                    {
+                        FirstName = fname,
+                        LastName = lname,
+                        Email = email,
+                        Password = password,
+                        District = district,
+                        ReadyForOvereposure = confirmation
+                    };
+                    context.Users.Add(user);
+                    context.SaveChanges();
+                    return user.UserId;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException?.Message);
+                    return 0;
+                }
+            }
+        }
+    }
+}
--- PetCareDB/PetCareDB/Program.cs
+++ PetCareDB/PetCareDB/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using PetCareDB.EF;
 using static System.Console;
+using System.Net;
+using System.Net.Sockets;
 
 namespace PetCareDB
 {
@@ -13,6 +15,39 @@ namespace PetCareDB
         static void Main(string[] args)
         {
 
+            /*
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            tcpSocket.Bind(tcpEndPoint);
+            tcpSocket.Listen(6);
+
+            while (true)
+            {
+                var listener = tcpSocket.Accept();
+                var buffer = new byte[256];
+                var size = 0;
+                var data = new StringBuilder();
+
+                do
+                {
+                    size = listener.Receive(buffer);
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                }
+                while (listener.Available > 0);
+
+                WriteLine(data.ToString());
+                WriteLine();
+                listener.Send(Encoding.UTF8.GetBytes("Всё успешно!"));
+
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
+            */
         }
     }
 }
